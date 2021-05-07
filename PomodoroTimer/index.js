3c211ae3f411cea7405ba19735a55cff265e1f8b
Iann Guerra <guerraiann@gmail.com>
2021-05-07 00:20:50 -0300

    starting html and js

---
+++ PomodoroTimer/index.js
@@ -0,0 +1,20 @@
+var pomodoro = {
+    started : false,
+    minutes : 0,
+    seconds : 0,
+    fillerHeight : 0,
+    fillerIncrement : 0,
+    interval : null,
+    minutesDom : null,
+    secondsDom : null,
+    fillerDom : null,
+    init : function(){
+      var self = this;
+      this.minutesDom = document.querySelector('#minutes');
+      this.secondsDom = document.querySelector('#seconds');
+      this.fillerDom = document.querySelector('#filler');
+      this.interval = setInterval(function(){
+        self.intervalCallback.apply(self);
+      }, 1000);
+    }
+}
